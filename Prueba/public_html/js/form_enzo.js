45150eb9b4a134ef6508a1a0e6747372d12dd88e
Enzo Toscano <eltano126@gmail.com>
2014-01-22 01:18:28 -0300

    enzo desple

--- Prueba/public_html/js/form_enzo.js
+++ Prueba/public_html/js/form_enzo.js
@@ -50,20 +50,21 @@ function mostrarXml(){ //Agregado para diferenciar las funciones
 }
 
 function desplegar(tabla_a_desplegar,estadoT, estadoTfila) {
-                var tablA = document.getElementById(tabla_a_desplegar);
-                var estadOt = document.getElementById(estadoT);
-                var fila = document.getElementById(estadoTfila);
+    
+    var tablA = document.getElementById(tabla_a_desplegar);
+    var estadOt = document.getElementById(estadoT);
+    var fila = document.getElementById(estadoTfila);
 
-                switch(tablA.style.display) {
-                    case "none":
-                        tablA.style.display = "block";
-                        estadOt.innerHTML = "Ocultar";
-                        fila.innerHTML = "";
-                        fila.innerHTML = " La fila de id: "+fila.id+". Contiene: "+fila.innerHTML+" :: "+fila.style.background;
-                        break;
-                    default:
-                        tablA.style.display = "none";
-                        estadOt.innerHTML = "Mostrar"
-                        break;
-                }
+    switch(tablA.style.display) {
+        case "none":
+            tablA.style.display = "block";
+            estadOt.innerHTML = "Ocultar";
+            fila.innerHTML = "";
+            fila.innerHTML = " La fila de id: "+fila.id+". Contiene: "+fila.innerHTML+" :: "+fila.style.background;
+            break;
+        default:
+            tablA.style.display = "none";
+            estadOt.innerHTML = "Mostrar";
+            break;
+    }
 }
